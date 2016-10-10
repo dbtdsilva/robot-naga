46ad39e93732bee4420d74bad5e615ffc96e69ce
Diogo Silva <dbtds@ua.pt>
2016-10-10 17:40:25 +0100

    Updated scripts

--- microrato/run.sh
+++ microrato/run.sh
@@ -1,3 +1,8 @@
-pcompile dumb-robot.c rmi-mr32.c
-ldpic32 -w dumb-robot.hex -p $1
-pterm -p $1
+set -o xtrace
+
+device=${1:-/dev/ttyUSB0}
+file=${2:-dumb-robot}
+
+pcompile $file.c rmi-mr32.c
+ldpic32 -w $file.hex -p $device
+pterm -p $device
--- microrato/run_bluetooth.sh
+++ microrato/run_bluetooth.sh
@@ -1,3 +1,8 @@
-pcompile dumb-robot.c rmi-mr32.c bluetooth_comm.c
-ldpic32 -w dumb-robot.hex -p $1
+set -o xtrace
+
+device=${1:-/dev/ttyUSB0}
+file=${2:-dumb-robot}
+
+pcompile $file.c rmi-mr32.c bluetooth_comm.c
+ldpic32 -w $file.hex -p $device
 pterm -p /dev/rfcomm0

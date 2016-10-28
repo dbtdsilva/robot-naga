9348c098763ce4ece1decb87959a5b99a94f2775
Diogo Silva <dbtds@ua.pt>
2016-10-29 00:04:06 +0100

    Improved boilerplate C++

---
+++ pathfinder/razernaga/RazerNaga.h
@@ -0,0 +1,30 @@
+//
+// Created by myrddin on 28/10/16.
+//
+
+#ifndef RAZERNAGA_RAZERNAGA_H
+#define RAZERNAGA_RAZERNAGA_H
+
+#include <string>
+#include <vector>
+#include <qapplication.h>
+
+class RazerNaga : public QApplication {
+    Q_OBJECT
+public:
+    RazerNaga(int &argc, char* argv[]);
+    RazerNaga(int &argc, char* argv[], int grid_position);
+    RazerNaga(int &argc, char* argv[], int grid_position, std::string host);
+    RazerNaga(int &argc, char* argv[], int grid_position, std::string host, std::vector<double> ir_sensor_angles);
+
+public slots:
+    void act();
+private:
+    const std::string name;
+    const int grid_position;
+    const std::string host;
+    const std::vector<double> ir_sensor_angles;
+};
+
+
+#endif //RAZERNAGA_RAZERNAGA_H
--- pathfinder/razernaga/main.cpp
+++ pathfinder/razernaga/main.cpp
@@ -34,183 +34,14 @@
 
 #include <iostream>
 
+#include "RazerNaga.h"
+
 using std::cerr;
 
-#include <qapplication.h>
 #include "libRobSock/RobSock.h"
-#include "sampapp.h"
 #include "robview.h"
 
-
-/* Calculate the power of left and right motors */
-void DetermineAction(int beaconToFollow, double *lPow, double *rPow)
-{
-    static int counter=0;
-
-    static double left, right, center;
-    bool   beaconReady;
-    static struct beaconMeasure beacon;
-    static int    Ground;
-    static bool   Collision ;
-
-    /*Access to values from Sensors - Only ReadSensors() gets new values */
-    if(IsObstacleReady(LEFT))
-        left      = GetObstacleSensor(LEFT);
-    if(IsObstacleReady(RIGHT))
-        right     = GetObstacleSensor(RIGHT);
-    if(IsObstacleReady(CENTER))
-        center    = GetObstacleSensor(CENTER);
-
-    beaconReady = IsBeaconReady(beaconToFollow);
-    if(beaconReady) {
-        beacon    = GetBeaconSensor(beaconToFollow);
-    }
-
-    if(IsGroundReady())
-        Ground    = GetGroundSensor();
-    if(IsBumperReady())
-        Collision = GetBumperSensor();
-
-    if(center>4.5 || right>4.5 || left>4.5 || Collision) { /* Close Obstacle - Rotate */
-        if(counter % 400 < 200) {
-            *lPow=0.06;
-            *rPow=-0.06; }
-        else {
-            *lPow=-0.06;
-            *rPow=0.06; }
-    }
-    else if(right>1.5) { /* Obstacle Near - Avoid */
-        *lPow=0.0;
-        *rPow=0.05;
-    }
-    else if(left>1.5) {
-        *lPow=0.05;
-        *rPow=0.0;
-    }
-    else {
-        if(beaconReady && beacon.beaconVisible) {
-            if(beacon.beaconDir>20.0) { /* turn to Beacon */
-                *lPow=0.0;
-                *rPow=0.1;
-            }
-            else if(beacon.beaconDir<-20.0) {
-                *lPow=0.1;
-                *rPow=0.0;
-            }
-            else { /* Full Speed Ahead */
-                *lPow=0.1;
-                *rPow=0.1;
-            }
-        }
-        else { /* Full Speed Ahead */
-            *lPow=0.1;
-            *rPow=0.1;
-        }
-    }
-
-    counter++;
-}
-
-/** SampApp methods **/
-
-SampApp::SampApp(int &argc, char*argv[], char *robot_name) : QApplication(argc,argv)
-{
-    strncpy(rob_name, robot_name, 19);
-    printf("What created!\n");
-    rob_name[19]='\0';
-
-    beaconToFollow = 0; // start by finding target at beacon 0
-}
-
-#define RUN        1
-#define STOP       2
-#define WAIT       3
-#define RETURN     4
-#define FINISHED   5
-
-void SampApp::act(void)
-{
-    printf("Act...\n");
-    static int state=STOP,stoppedState=RUN;
-    double lPow, rPow;
-
-    ReadSensors();
-
-    if(GetFinished()) /* Simulator has received Finish() or Robot Removed */
-    {
-        printf("%s Exiting %d\n",rob_name, GetTime());
-        exit(0);
-        return;
-    }
-    if(state==STOP && GetStartButton()) state=stoppedState;  /* Start     */
-    if(state!=STOP && GetStopButton())  {
-        stoppedState=state;
-        state=STOP; /* Interrupt */
-    }
-
-    switch (state) {
-        case RUN:    /* Go */
-            if( GetVisitingLed() ) state = WAIT;
-            if(GetGroundSensor()==0) {         /* Visit Target */
-                SetVisitingLed(true);
-                printf("%s visited target at %d\n", rob_name, GetTime());
-            }
-
-            else {
-                DetermineAction(0,&lPow,&rPow);
-                DriveMotors(lPow,rPow);
-            }
-            break;
-        case WAIT: /* Wait for others to visit target */
-            SetReturningLed(true);
-            if(GetVisitingLed()) SetVisitingLed(false);
-            if(GetReturningLed()) state = RETURN;
-            DriveMotors(0.0,0.0);
-            break;
-        case RETURN: /* Return to home area */
-            if(GetVisitingLed()) SetVisitingLed(false);
-            SetReturningLed(false);
-
-            // Wander
-            DetermineAction(1,&lPow,&rPow);
-            DriveMotors(lPow,rPow);
-
-            break;
-
-    }
-
-
-    //Request Sensors for next cycle
-    /*** Not needed in RMI
- if(GetTime() % 2 == 0) {
-       RequestObstacleSensor(CENTER);
-
-       if(GetTime() % 8 == 0 || state == RETURN)
-           RequestGroundSensor();
-       else
-           RequestBeaconSensor(beaconToFollow);
-
-    }
-    else {
-       RequestSensors(2, "IRSensor1", "IRSensor2");
-    }
-
-
-    // Communication
-Say(rob_name);
-    ***/
-
-
-//         for(int r=1; r<=6; r++) {
-//             if(NewMessageFrom(r)) {
-//		printf("%s: Message From %d \"%s\"\n", rob_name, r, GetMessageFrom(r));
-//             }
-//         }
-
-}
-
-
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
     char host[100]="localhost";
     char rob_name[20]="GUISample";
@@ -252,23 +83,7 @@ int main( int argc, char** argv )
         return 1;
     }
 
-    // Create Qt application - Must be before InitRobot
-    SampApp app( argc, argv, rob_name );
-    qApp->addLibraryPath("libRobSock");
-
-    /* Connect Robot to simulator */
-    double irSensorAngles[4] = { 0.0, 60.0, -60.0, 180.0};
-    printf("What\n");
-    if(InitRobot2(rob_name,rob_id,irSensorAngles,host)==-1) {
-        printf("%s Failed to connect\n",rob_name);
-        return 1;
-    }
-    printf("What2\n");
-    printf("%s Connected\n",rob_name);
-    fflush(stdout);
-
-    // Connect event NewMessage to handler act()
-    QObject::connect((QObject *)(Link()), SIGNAL(NewMessage()), &app, SLOT(act()));
+    RazerNaga app(argc, argv);
 
     // create robot display widget
     //RobView robView(irSensorAngles, rob_name);
